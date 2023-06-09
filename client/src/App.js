365f4af837b5f09c1000f295b1ee44da6683abb1
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-09 11:07:28 -0400

    Created a layout for adding a header and footer to components

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,44 @@ import {
     createBrowserRouter,
     RouterProvider,
     Route,
+    Outlet
 } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Single from "./pages/Single";
 import Write from "./pages/Write";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+    return (
+        <>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </>
+    )
+}
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home/>,
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/single",
+                element: <Single/>
+            },
+            {
+                path: "/write",
+                element: <Write/>
+            }
+        ]
     },
     {
         path: "/login",
@@ -22,15 +49,7 @@ const router = createBrowserRouter([
     {
         path: "/register",
         element: <Register/>,
-    },
-    {
-        path: "/single",
-        element: <Single/>,
-    },
-    {
-        path: "/write",
-        element: <Write/>,
-    },
+    }
 ]);
 
 function App() {
@@ -42,3 +61,5 @@ function App() {
 }
 
 export default App;
+
+
