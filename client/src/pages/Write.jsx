9d054e3a62382a3dcdcd101a6b9de753eb1506e0
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-10 20:47:43 -0400

    Added elements to the Write page

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+    const [value, setValue] = useState("");
+
     return (
-        <div>
-            
+        <div className='write'>
+            <div className="content">
+                <input type="text" placeholder='Title'/>
+                <div className="editorContainer">
+                    <ReactQuill theme="snow" value={value} onChange={setValue} />
+                </div>
+            </div>
+            <div className="menu">
+                <div className="item">1</div>
+                <div className="item">2</div>
+            </div>
         </div>
     );
 };
