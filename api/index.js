2bd683819959ede008e2269c7754bdd3fdf900f5
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-14 10:02:03 -0400

    Basic setup for routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import router from './routes/posts.js';
+import postRoutes from './routes/posts.js';
+import authRoutes from './routes/auth.js';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api/posts", router);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(8000, () => {
     console.log(`Connected`);
