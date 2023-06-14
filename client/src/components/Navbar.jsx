1e9d02809544f0eb381d505033d6c6d780701f78
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-14 12:34:49 -0400

    Added user name to the application navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 
 const Navbar = () => {
+    const { currentUser } = useContext(AuthContext);
+
     return (
         <div className='navbar'>
             <div className="container">
@@ -28,7 +31,7 @@ const Navbar = () => {
                     <Link className='links' to='/?cat=food'>
                         <h6>FOOD</h6>
                     </Link>
-                    <span>John</span>
+                    <span>{currentUser?.username}</span>
                     <span>Logout</span>
                     <span className='write'>
                         <Link className='link' to='/write'>Write</Link>
