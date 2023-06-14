f2db6b17cf156f0d3e67f9bbec68e1e61cbcbe6b
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-14 14:53:50 -0400

    Sorted and showed categories based on the selection

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,4 +1,32 @@
+import { db } from "../db.js";
+// import jwt from "jsonwebtoken";
+
+export const getPosts = (request, response) => {
+    const q = request.query.cat
+        ? "SELECT * FROM posts WHERE cat=?"
+        : "SELECT * FROM posts";
+
+    db.query(q, [request.query.cat], (err, data) => {
+        if (err) return response.status(500).send(err);
+
+        return response.status(200).json(data);
+    });
+};
+
+export const getPost = (request, response) => {
+    response.json("from controller");
+};
+
 export const addPost = (request, response) => {
     response.json("from controller");
 };
+
+export const deletePost = (request, response) => {
+    response.json("from controller");
+};
+
+export const updatePost = (request, response) => {
+    response.json("from controller");
+};
+
 
