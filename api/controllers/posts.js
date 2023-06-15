961162ecabd98560ca3c6ee0eb8befccb684fa57
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-06-15 12:39:59 -0400

    Added deletion functionality for the users posts

--- api/controllers/posts.js
+++ api/controllers/posts.js
@@ -1,4 +1,5 @@
 import { db } from "../db.js";
+import jwt from "jsonwebtoken";
 // import jwt from "jsonwebtoken";
 
 export const getPosts = (request, response) => {
@@ -14,7 +15,14 @@ export const getPosts = (request, response) => {
 };
 
 export const getPost = (request, response) => {
-    response.json("from controller");
+    const q =
+        "SELECT p.id, `username`, `title`, `description`, p.image, u.image AS userImage, `date`,`cat` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
+    db.query(q, [request.params.id], (err, data) => {
+        if (err) return response.status(500).json(err);
+
+        return response.status(200).json(data[0]);
+    });
 };
 
 export const addPost = (request, response) => {
@@ -22,7 +30,21 @@ export const addPost = (request, response) => {
 };
 
 export const deletePost = (request, response) => {
-    response.json("from controller");
+    const token = request.cookies.access_token;
+    if (!token) return response.status(401).json("Not authenticated!");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if (err) return response.status(403).json("Token is not valid!");
+
+        const postId = request.params.id;
+        const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+
+        db.query(q, [postId, userInfo.id], (err, data) => {
+            if (err) return response.status(403).json("You can delete only your post!");
+
+            return response.json("Post has been deleted!");
+        });
+    });
 };
 
 export const updatePost = (request, response) => {
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,36 +1,68 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 
 import EditImage from "../images/edit.png";
 import DeleteImage from "../images/delete.png";
-import {Link} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import {AuthContext} from "../context/authContext";
 
 const Single = () => {
+    const [post, setPost] = useState({});
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const postId = location.pathname.split("/")[2];
+
+    const {currentUser} = useContext(AuthContext);
+
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`/posts/${postId}`);
+            navigate("/")
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(`/posts/${postId}`);
+                setPost(res.data);
+            } catch (err) {
+                console.log(`Error`,err);
+            }
+        };
+        fetchData();
+    }, [postId]);
+
     return (
         <div className='single'>
             <div className="content">
                 <img src="https://images.pexels.com/photos/14245014/pexels-photo-14245014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
                 <div className="user">
-                    <img src="https://images.pexels.com/photos/2449543/pexels-photo-2449543.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+                    <img src={post?.userImage} alt=""/>
                     <div className="info">
-                        <span>John</span>
-                        <p>Posted 2 days ago</p>
+                        <span>{post.username}</span>
+                        <p>Posted {moment(post.date).fromNow()}</p>
                     </div>
-                    <div className="edit">
-                        <Link to={`/write?edit=2`}>
-                            <img src={ EditImage } alt=""/>
-                        </Link>
-                        <img src={ DeleteImage } alt=""/>
-                    </div>
+                    {
+                        currentUser.username === post.username
+                        &&
+                        <div className="edit">
+                            <Link to={`/write?edit=2`}>
+                                <img src={ EditImage } alt=""/>
+                            </Link>
+                            <img  onClick={handleDelete} src={ DeleteImage } alt=""/>
+                        </div>
+                    }
+
                 </div>
-                <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, velit!</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi delectus deleniti
-                    dignissimos earum eligendi eos ex, fugit id modi natus neque nostrum porro quae, quo repellat
-                    repellendus saepe sint soluta sunt tempore, temporibus veritatis vero. Accusantium eveniet
-                    exercitationem explicabo fuga laudantium! Consectetur consequuntur corporis culpa laborum
-                    laudantium provident, voluptates.
-                </p>
+                <h1>{post.title}</h1>
+                    {post.description}
             </div>
             <Menu/>
         </div>
